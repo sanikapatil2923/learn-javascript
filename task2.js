decc17cd60eb432e0d89d9a52cf8a52bc000f026
sanikapatil2923 <patilsanika2904@gmail.com>
2026-06-21 10:51:31 +0530

    Add files via upload

---
+++ task2.js
@@ -0,0 +1,11 @@
+const profile = {
+  name: "sanika patil",
+  year: "4th",
+  headline: "Aspiring Data Scientist | Artificial Intelligence | Machine Learning",
+  college: "Adarsh Institute Vita",
+  campus: "Adarsh campus",
+  location: "Vita, Maharashtra, India",
+  connections: "500+"
+};
+
+console.log(profile);
